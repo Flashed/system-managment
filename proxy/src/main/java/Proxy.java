e60a63fc867ebb08219cfe722a65e9585df6f046
Mikhail Z <zaitsev@primetver.ru>
2014-11-25 17:52:35 +0300

    Using Acceptor in Proxy

--- proxy/src/main/java/Proxy.java
+++ proxy/src/main/java/Proxy.java
@@ -1,9 +1,47 @@
 
-public class Proxy {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.system.managment.common.socket.acceptor.Acceptor;
+import ru.system.managment.common.socket.acceptor.AcceptorListener;
+import ru.system.managment.common.socket.model.SocketData;
+
+import java.nio.channels.SocketChannel;
+
+public class Proxy implements Runnable, AcceptorListener{
+
+  private static final Logger logger = LoggerFactory.getLogger(Proxy.class);
+
+  private Acceptor acceptor;
+
+
+  @Override
+  public void run() {
+    try {
+      acceptor.start();
+    } catch (Exception e) {
+      throw new RuntimeException();
+    }
+  }
+
+  public void setAcceptor(Acceptor acceptor) {
+    this.acceptor = acceptor;
+  }
 
   public static void main(String... args){
+    try{
+
+    }catch (Exception e){
+      logger.error("Error of application", e);
+    }
+  }
+
+  @Override
+  public void onAccept(SocketChannel channel) {
 
   }
 
-  
+  @Override
+  public void onRead(SocketData data) {
+
+  }
 }
